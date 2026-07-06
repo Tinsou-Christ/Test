2b31b8cc06be44439fec6eb9a50680dfaa58d128
Tinsou-Christ <anosvoldigo291@gmail.com>
2026-07-06 09:44:52 +0000

    Add lyrics command handler to main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from handlers.alldl import alldl_command
 from handlers.gem import gem_command
 from handlers.pinterest import pinterest_command, pinterest_next_callback
 from handlers.lifeai import lifeai_command
+from handlers.lyrics import lyrics_command
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -47,6 +48,7 @@ def main():
     app.add_handler(CommandHandler(['alldl', 'dl'], alldl_command))
     app.add_handler(CommandHandler('gem', gem_command))
     app.add_handler(CommandHandler(['pinterest', 'pin'], pinterest_command))
+    app.add_handler(CommandHandler(['lyrics', 'songlyrics'], lyrics_command))
     app.add_handler(CallbackQueryHandler(pinterest_next_callback, pattern=r'^pin_next:'))
 
     logger.info('Bot demarre, polling en cours...')
